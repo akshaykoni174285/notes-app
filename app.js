660106fae8598c4dcf36ffe12af4535b4bfcd48f
akshaykoni174285 <akshaykoni174285@gmail.com>
2024-07-21 14:17:42 +0530

    intro

---
+++ app.js
@@ -0,0 +1,6 @@
+const add = require('./util.js');
+console.log("hello")
+
+const res = add(3,5)
+
+console.log(res)
---
+++ util.js
@@ -0,0 +1,8 @@
+console.log("from util file")
+var name= "akshay"
+
+const add = function(num1,num2){
+   return num1 + num2
+}
+
+module.exports = add 
