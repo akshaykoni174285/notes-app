46bfd14dee16fdf84bf157a4b926ef32b3e66bf7
akshaykoni174285 <akshaykoni174285@gmail.com>
2024-07-23 01:02:27 +0530

    intro

--- app.js
+++ app.js
@@ -7,17 +7,31 @@ import {hideBin} from 'yargs/helpers'
 
 
 const argv = yargs(hideBin(process.argv))
-.command('add','add a note',()=>{
-    console.log("adding a new note");
-})
-.command('remove','remove a note',()=>{
-    console.log("removing a note");
-})
-.help()
-.argv;
+    .command('add','add a note',(yargs)=>{
+        return yargs
+        .option('title',{
+            describe: 'title of the note',
+            type: 'string',
+            demandOption:true
+
+        })
+    },(argv)=>{
+        console.log(`adding a new note with title ${argv.title}`);
+    })
+    .command('remove','remove a note',()=>{
+        console.log("removing a note");
+    })
+    .command('list','listing the notes',()=>{
+        console.log("listing the notes");
+    })
+    .command('read','reading a note',()=>{
+        console.log("reading a note");
+    })
+    .help()
+    .argv;
 
 
 const mesg = getNotes();
-console.log(mesg);
+// console.log(mesg);
 
-console.log(argv)
+// console.log(argv)
